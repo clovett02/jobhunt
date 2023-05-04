c5412a2a674fe9077b3eb6df89a4807b6b91bf23
Chris Lovett <chris.lovett02@outlook.com>
2023-05-04 02:30:29 -0400

    added Jobs file and class for Jobs page

---
+++ src/components/Jobs.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Jobs extends Component
+{
+    static displayName = Jobs.name;
+
+    constructor(props)
+    {
+        super(props);
+        
+    }
+
+    render () 
+    {
+        return 
+        (
+            <div>
+
+            </div>
+        );
+    }
+}
