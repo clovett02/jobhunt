f3d251013823c60e9d2011c6085efa197166838f
Chris Lovett <chris.lovett02@outlook.com>
2025-03-30 01:57:06 -0400

    Switched to html and css navbar
    Switched to a standard html and css navbar instead of the old,
    unreliable react, prebuilt, component solution that I tried to
    use so long ago.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+// import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+// import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -23,27 +23,24 @@ export class NavMenu extends Component {
 
   render() {
     return (
-      <header>
-        <ul>
-          <li>
-            
-          </li>
-        </ul>
-
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">JobHunt</NavbarBrand>
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className='text-dark' to="/add-job">Add Job</NavLink>
-              </NavItem>
+      <div className='headarea'>
+          <div className='brand'>
+            <a href='/'>Job Hunt</a>
+          </div>
+          <div className='links'>
+            <ul>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='/add-job'>Add Job</a>
+              </li>
             </ul>
-          </Collapse>
-        </Navbar>
-      </header>
+          </div>
+          <br/>
+          <br/>
+          {/* <hr className='divider'></hr> */}
+        </div>
     );
   }
 }
